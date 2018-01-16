cabceb28035c13cf7a9a21c2fdf2dbb9b31229d3
lilyMonika <123>
2018-01-15 18:38:06 -0800

    Added member class and a MemberController

---
+++ TeamSourceControlProject/TeamSourceControlProject/Models/Member.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamSourceControlProject.Models
+{
+    public class Member
+    {
+        //TODO: Add properties to member
+    }
+}
