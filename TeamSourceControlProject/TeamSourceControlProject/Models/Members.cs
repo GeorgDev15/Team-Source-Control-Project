e70004b804d63613227a00cc0832f3ad5924f787
JasperLawliet <JasperLawliet@hotmail.com>
2018-01-16 21:30:39 -0800

    Added Members class, includes MemberID, First and last name, birth day, and favorite animal properties.

---
+++ TeamSourceControlProject/TeamSourceControlProject/Models/Members.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamSourceControlProject.Models
+{
+    /// <summary>
+    /// contains basic information for a member to be added into simple database.
+    /// </summary>
+    public class Members
+    {
+        public int MemberID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string FavoriteAnimal { get; set; }
+    }
+}
