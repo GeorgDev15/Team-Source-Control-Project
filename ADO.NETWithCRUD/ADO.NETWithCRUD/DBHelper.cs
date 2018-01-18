57fa3bed93d024968ec7c3beb8c9594d2d3f7517
lilyMonika <123>
2018-01-17 16:15:33 -0800

    Added DBHelper class

---
+++ ADO.NETWithCRUD/ADO.NETWithCRUD/DBHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NETWithCRUD
+{
+    class DBHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            //TODO: Add connection link
+            return new SqlConnection("")
+        }
+    }
+}
