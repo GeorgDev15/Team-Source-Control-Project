dd1d72846807cb289ed94feb93a6bd3881d4230f
JasperLawliet <JasperLawliet@hotmail.com>
2018-01-17 20:41:48 -0800

    Added basic UI elements. TODO: create form for register and update info.

--- ADO.NETWithCRUD/ADO.NETWithCRUD/Form1.Designer.cs
+++ ADO.NETWithCRUD/ADO.NETWithCRUD/Form1.Designer.cs
@@ -28,20 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstBoxRegisteredUsers = new System.Windows.Forms.ListBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lstBoxRegisteredUsers
+            // 
+            this.lstBoxRegisteredUsers.FormattingEnabled = true;
+            this.lstBoxRegisteredUsers.ItemHeight = 16;
+            this.lstBoxRegisteredUsers.Location = new System.Drawing.Point(70, 194);
+            this.lstBoxRegisteredUsers.Name = "lstBoxRegisteredUsers";
+            this.lstBoxRegisteredUsers.Size = new System.Drawing.Size(443, 228);
+            this.lstBoxRegisteredUsers.TabIndex = 0;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(47, 25);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(136, 43);
+            this.btnRegister.TabIndex = 1;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(398, 25);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(136, 43);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update Info";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(47, 125);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(136, 43);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(398, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 361);
+            this.ClientSize = new System.Drawing.Size(589, 444);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lstBoxRegisteredUsers);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Database Form";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstBoxRegisteredUsers;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ADO.NETWithCRUD/ADO.NETWithCRUD/Form1.cs
+++ ADO.NETWithCRUD/ADO.NETWithCRUD/Form1.cs
@@ -16,5 +16,10 @@ namespace ADO.NETWithCRUD
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
