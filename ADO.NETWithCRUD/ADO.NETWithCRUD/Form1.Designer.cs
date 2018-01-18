6a59c6c1077b7f55f9465767d7a8242e1d50ff8e
JasperLawliet <JasperLawliet@hotmail.com>
2018-01-17 21:08:35 -0800

    added label above listbox.

--- ADO.NETWithCRUD/ADO.NETWithCRUD/Form1.Designer.cs
+++ ADO.NETWithCRUD/ADO.NETWithCRUD/Form1.Designer.cs
@@ -33,13 +33,14 @@
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lstBoxRegisteredUsers
             // 
             this.lstBoxRegisteredUsers.FormattingEnabled = true;
             this.lstBoxRegisteredUsers.ItemHeight = 16;
-            this.lstBoxRegisteredUsers.Location = new System.Drawing.Point(70, 194);
+            this.lstBoxRegisteredUsers.Location = new System.Drawing.Point(72, 204);
             this.lstBoxRegisteredUsers.Name = "lstBoxRegisteredUsers";
             this.lstBoxRegisteredUsers.Size = new System.Drawing.Size(443, 228);
             this.lstBoxRegisteredUsers.TabIndex = 0;
@@ -84,11 +85,21 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Registered users";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(589, 444);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnUpdate);
@@ -98,6 +109,7 @@
             this.Name = "Form1";
             this.Text = "Database Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,6 +120,7 @@
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
