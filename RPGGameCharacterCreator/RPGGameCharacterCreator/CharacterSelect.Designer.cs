ed1083b4651a26ffeed2a867257e253f646db52c
Selvaria <Selvaria@HANAKO>
2018-03-11 19:22:26 -0700

    Create a character

--- RPGGameCharacterCreator/RPGGameCharacterCreator/CharacterSelect.Designer.cs
+++ RPGGameCharacterCreator/RPGGameCharacterCreator/CharacterSelect.Designer.cs
@@ -28,8 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.BtnSelect = new System.Windows.Forms.Button();
+            this.BtnCreate = new System.Windows.Forms.Button();
             this.BtnRandom = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.LblAllocation = new System.Windows.Forms.Label();
@@ -38,29 +37,27 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.TxtVitality = new System.Windows.Forms.TextBox();
+            this.TxtDefense = new System.Windows.Forms.TextBox();
+            this.TxtStrength = new System.Windows.Forms.TextBox();
+            this.BtnAddVIT = new System.Windows.Forms.Button();
+            this.BtnAddSTR = new System.Windows.Forms.Button();
+            this.BtnAddDEF = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // BtnCreate
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Character Creation";
-            // 
-            // BtnSelect
-            // 
-            this.BtnSelect.Location = new System.Drawing.Point(266, 222);
-            this.BtnSelect.Name = "BtnSelect";
-            this.BtnSelect.Size = new System.Drawing.Size(75, 23);
-            this.BtnSelect.TabIndex = 2;
-            this.BtnSelect.Text = "Select";
-            this.BtnSelect.UseVisualStyleBackColor = true;
+            this.BtnCreate.Location = new System.Drawing.Point(93, 222);
+            this.BtnCreate.Name = "BtnCreate";
+            this.BtnCreate.Size = new System.Drawing.Size(75, 23);
+            this.BtnCreate.TabIndex = 2;
+            this.BtnCreate.Text = "Create";
+            this.BtnCreate.UseVisualStyleBackColor = true;
             // 
             // BtnRandom
             // 
-            this.BtnRandom.Location = new System.Drawing.Point(15, 222);
+            this.BtnRandom.Location = new System.Drawing.Point(12, 222);
             this.BtnRandom.Name = "BtnRandom";
             this.BtnRandom.Size = new System.Drawing.Size(75, 23);
             this.BtnRandom.TabIndex = 3;
@@ -70,7 +67,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(206, 9);
+            this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(90, 13);
             this.label2.TabIndex = 4;
@@ -79,7 +76,7 @@
             // LblAllocation
             // 
             this.LblAllocation.AutoSize = true;
-            this.LblAllocation.Location = new System.Drawing.Point(292, 9);
+            this.LblAllocation.Location = new System.Drawing.Point(108, 9);
             this.LblAllocation.Name = "LblAllocation";
             this.LblAllocation.Size = new System.Drawing.Size(19, 13);
             this.LblAllocation.TabIndex = 5;
@@ -88,33 +85,33 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Location = new System.Drawing.Point(22, 87);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "HP: ";
+            this.label3.Text = "Vitality: ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(12, 170);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.Size = new System.Drawing.Size(53, 13);
             this.label4.TabIndex = 7;
-            this.label4.Text = "HP: ";
+            this.label4.Text = "Defense: ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 128);
+            this.label5.Location = new System.Drawing.Point(12, 129);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.Size = new System.Drawing.Size(53, 13);
             this.label5.TabIndex = 8;
-            this.label5.Text = "HP: ";
+            this.label5.Text = "Strength: ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(99, 52);
+            this.textBox1.Location = new System.Drawing.Point(108, 44);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 9;
@@ -122,17 +119,81 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 55);
+            this.label6.Location = new System.Drawing.Point(12, 47);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(90, 13);
             this.label6.TabIndex = 10;
             this.label6.Text = "Character Name: ";
             // 
+            // TxtVitality
+            // 
+            this.TxtVitality.Location = new System.Drawing.Point(71, 84);
+            this.TxtVitality.Name = "TxtVitality";
+            this.TxtVitality.Size = new System.Drawing.Size(29, 20);
+            this.TxtVitality.TabIndex = 11;
+            // 
+            // TxtDefense
+            // 
+            this.TxtDefense.Location = new System.Drawing.Point(71, 167);
+            this.TxtDefense.Name = "TxtDefense";
+            this.TxtDefense.Size = new System.Drawing.Size(31, 20);
+            this.TxtDefense.TabIndex = 12;
+            // 
+            // TxtStrength
+            // 
+            this.TxtStrength.Location = new System.Drawing.Point(71, 126);
+            this.TxtStrength.Name = "TxtStrength";
+            this.TxtStrength.Size = new System.Drawing.Size(31, 20);
+            this.TxtStrength.TabIndex = 13;
+            // 
+            // BtnAddVIT
+            // 
+            this.BtnAddVIT.Location = new System.Drawing.Point(108, 82);
+            this.BtnAddVIT.Name = "BtnAddVIT";
+            this.BtnAddVIT.Size = new System.Drawing.Size(20, 23);
+            this.BtnAddVIT.TabIndex = 14;
+            this.BtnAddVIT.Text = "+";
+            this.BtnAddVIT.UseVisualStyleBackColor = true;
+            // 
+            // BtnAddSTR
+            // 
+            this.BtnAddSTR.Location = new System.Drawing.Point(108, 123);
+            this.BtnAddSTR.Name = "BtnAddSTR";
+            this.BtnAddSTR.Size = new System.Drawing.Size(20, 23);
+            this.BtnAddSTR.TabIndex = 15;
+            this.BtnAddSTR.Text = "+";
+            this.BtnAddSTR.UseVisualStyleBackColor = true;
+            // 
+            // BtnAddDEF
+            // 
+            this.BtnAddDEF.Location = new System.Drawing.Point(108, 164);
+            this.BtnAddDEF.Name = "BtnAddDEF";
+            this.BtnAddDEF.Size = new System.Drawing.Size(20, 23);
+            this.BtnAddDEF.TabIndex = 16;
+            this.BtnAddDEF.Text = "+";
+            this.BtnAddDEF.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // CreateCharacter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(353, 257);
+            this.ClientSize = new System.Drawing.Size(260, 257);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnAddDEF);
+            this.Controls.Add(this.BtnAddSTR);
+            this.Controls.Add(this.BtnAddVIT);
+            this.Controls.Add(this.TxtStrength);
+            this.Controls.Add(this.TxtDefense);
+            this.Controls.Add(this.TxtVitality);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
@@ -141,8 +202,7 @@
             this.Controls.Add(this.LblAllocation);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.BtnRandom);
-            this.Controls.Add(this.BtnSelect);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCreate);
             this.Name = "CreateCharacter";
             this.Text = "Create a Character";
             this.ResumeLayout(false);
@@ -151,9 +211,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button BtnSelect;
+        private System.Windows.Forms.Button BtnCreate;
         private System.Windows.Forms.Button BtnRandom;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label LblAllocation;
@@ -162,5 +220,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtVitality;
+        private System.Windows.Forms.TextBox TxtDefense;
+        private System.Windows.Forms.TextBox TxtStrength;
+        private System.Windows.Forms.Button BtnAddVIT;
+        private System.Windows.Forms.Button BtnAddSTR;
+        private System.Windows.Forms.Button BtnAddDEF;
+        private System.Windows.Forms.Button button1;
     }
 }
