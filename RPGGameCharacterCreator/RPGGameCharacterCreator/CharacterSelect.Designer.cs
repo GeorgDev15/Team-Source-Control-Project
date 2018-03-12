350d2b34540d61953188b597e5b879a015bc7acf
Selvaria <Selvaria@HANAKO>
2018-03-11 19:03:17 -0700

    Front-end UI

---
+++ RPGGameCharacterCreator/RPGGameCharacterCreator/CharacterSelect.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace RPGGameCharacterCreator
+{
+    partial class CreateCharacter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnSelect = new System.Windows.Forms.Button();
+            this.BtnRandom = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblAllocation = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Character Creation";
+            // 
+            // BtnSelect
+            // 
+            this.BtnSelect.Location = new System.Drawing.Point(266, 222);
+            this.BtnSelect.Name = "BtnSelect";
+            this.BtnSelect.Size = new System.Drawing.Size(75, 23);
+            this.BtnSelect.TabIndex = 2;
+            this.BtnSelect.Text = "Select";
+            this.BtnSelect.UseVisualStyleBackColor = true;
+            // 
+            // BtnRandom
+            // 
+            this.BtnRandom.Location = new System.Drawing.Point(15, 222);
+            this.BtnRandom.Name = "BtnRandom";
+            this.BtnRandom.Size = new System.Drawing.Size(75, 23);
+            this.BtnRandom.TabIndex = 3;
+            this.BtnRandom.Text = "Randomize";
+            this.BtnRandom.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Allocation points: ";
+            // 
+            // LblAllocation
+            // 
+            this.LblAllocation.AutoSize = true;
+            this.LblAllocation.Location = new System.Drawing.Point(292, 9);
+            this.LblAllocation.Name = "LblAllocation";
+            this.LblAllocation.Size = new System.Drawing.Size(19, 13);
+            this.LblAllocation.TabIndex = 5;
+            this.LblAllocation.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "HP: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "HP: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "HP: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Character Name: ";
+            // 
+            // CreateCharacter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 257);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblAllocation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnRandom);
+            this.Controls.Add(this.BtnSelect);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateCharacter";
+            this.Text = "Create a Character";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnSelect;
+        private System.Windows.Forms.Button BtnRandom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblAllocation;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
--- RPGGameCharacterCreator/RPGGameCharacterCreator/Form1.Designer.cs
+++ RPGGameCharacterCreator/RPGGameCharacterCreator/Form1.Designer.cs
@@ -28,12 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblCharacter2 = new System.Windows.Forms.Label();
+            this.BtnCharacterSelect1 = new System.Windows.Forms.Button();
+            this.BtnCharacterSelect2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.BtnFight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Character";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(294, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "VS";
+            // 
+            // LblCharacter2
+            // 
+            this.LblCharacter2.AutoSize = true;
+            this.LblCharacter2.Location = new System.Drawing.Point(508, 9);
+            this.LblCharacter2.Name = "LblCharacter2";
+            this.LblCharacter2.Size = new System.Drawing.Size(86, 13);
+            this.LblCharacter2.TabIndex = 2;
+            this.LblCharacter2.Text = "Select Character";
+            // 
+            // BtnCharacterSelect1
+            // 
+            this.BtnCharacterSelect1.Location = new System.Drawing.Point(16, 176);
+            this.BtnCharacterSelect1.Name = "BtnCharacterSelect1";
+            this.BtnCharacterSelect1.Size = new System.Drawing.Size(107, 23);
+            this.BtnCharacterSelect1.TabIndex = 3;
+            this.BtnCharacterSelect1.Text = "Character Select";
+            this.BtnCharacterSelect1.UseVisualStyleBackColor = true;
+            // 
+            // BtnCharacterSelect2
+            // 
+            this.BtnCharacterSelect2.Location = new System.Drawing.Point(476, 176);
+            this.BtnCharacterSelect2.Name = "BtnCharacterSelect2";
+            this.BtnCharacterSelect2.Size = new System.Drawing.Size(111, 23);
+            this.BtnCharacterSelect2.TabIndex = 4;
+            this.BtnCharacterSelect2.Text = "Character Select";
+            this.BtnCharacterSelect2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(67, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "HP:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(67, 79);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "STR:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(495, 44);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(441, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "HP:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(495, 82);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(441, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "STR:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(67, 116);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 119);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "DEF:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(495, 116);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(441, 119);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 13);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "DEF:";
+            // 
+            // BtnFight
+            // 
+            this.BtnFight.Location = new System.Drawing.Point(268, 85);
+            this.BtnFight.Name = "BtnFight";
+            this.BtnFight.Size = new System.Drawing.Size(75, 23);
+            this.BtnFight.TabIndex = 21;
+            this.BtnFight.Text = "Fight";
+            this.BtnFight.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(606, 220);
+            this.Controls.Add(this.BtnFight);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BtnCharacterSelect2);
+            this.Controls.Add(this.BtnCharacterSelect1);
+            this.Controls.Add(this.LblCharacter2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RPGGameCharacterCreator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblCharacter2;
+        private System.Windows.Forms.Button BtnCharacterSelect1;
+        private System.Windows.Forms.Button BtnCharacterSelect2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button BtnFight;
     }
 }
 
